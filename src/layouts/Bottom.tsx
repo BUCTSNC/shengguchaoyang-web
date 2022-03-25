d0de13b67a99752a9511329d166835954df7ad19
Hexiang <qihexiang@outlook.com>
2022-03-25 11:07:39 +0800

    修正了一些错别字

--- src/layouts/Bottom.tsx
+++ src/layouts/Bottom.tsx
@@ -4,7 +4,7 @@ import { LightBlue, White } from '../ColorCard';
 export default function Bottom() {
     return <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around", alignItems: "center", backgroundColor: LightBlue, color: White }}>
         <div>北京化工大学Logo</div>
-        <div>学生网络总新Logo</div>
+        <div>学生网络中心Logo</div>
         <div style={{ textAlign: 'center' }}>
             <div>Copyright &copy; {new Date().getFullYear()} SNC All Right Reversed</div>
             <div>地址：北京市朝阳区北三环东路15号北京化工大学</div>
