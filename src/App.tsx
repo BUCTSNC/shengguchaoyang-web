6b339f5a9197c0d5740eab0137c199bccd41b47e
Hexiang <qihexiang@outlook.com>
2022-03-25 11:06:09 +0800

    修复了404页面在下方弹出的问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { Modal } from 'antd';
 
@@ -37,11 +37,13 @@ function App() {
         <SearchViewCtx.Provider value={searchViewSwitch}>
           <Router>
             <Navibar />
-            <Route path="/" exact component={HomePage} />
-            <Route path="/post/:postUrl*" component={Post} />
-            <Route path="/cate/:category*" component={CategoryPage} />
-            <Route path="/about" exact component={About} />
-            {/* <Route path="/*" component={NotFound} /> */}
+            <Switch>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/post/:postUrl*" component={Post} />
+              <Route path="/cate/:category*" component={CategoryPage} />
+              <Route path="/about" exact component={About} />
+              <Route path="*" component={NotFound} />
+            </Switch>
             <SearchBtn onClick={() => searchViewSwitch(true)} />
             <ScrollToTopBtn />
             <Modal
