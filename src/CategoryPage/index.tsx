ab6a936f7f45d04039b801a0d7995c9c59d3f5c3
Hexiang Qi <qihexiang@outlook.com>
2022-04-23 10:33:47 +0800

    adaptation topbar, bottom bar and category page for mobile platform

--- src/CategoryPage/index.tsx
+++ src/CategoryPage/index.tsx
@@ -7,62 +7,66 @@ import React from "react";
 import { Button } from "antd";
 import { PostCard } from "../components/PostCard";
 import Container, { ContainerNG } from "../components/Container";
-import "./CategoryPage.css"
+import "./CategoryPage.css";
 import { Feedback } from "../components/Feedback";
 
 export const CategoryPage = () => {
-    const history = useHistory();
-    const { category: categoryPath } = useParams<{ category: string; }>();
-    const categoriesChain = categoryPath.split("/")
-    const { cates } = climbTree(useContext(CateTree));
-    const [width,setWidth] = useState(window.innerWidth)
-    const parseCategory = (): Definitions.CategoryProps | undefined => {
-        const cate = cates.find(cate => cate.path === categoryPath);
-        return cate
-    };
-    const [categoryInfo, setCategory] = useState<Definitions.CategoryProps | undefined>(parseCategory());
-    useEffect(() => {
-        setCategory(parseCategory);
-    }, [categoryPath, cates]);
-    useEffect(() => {
-        document.title = `${categoryInfo?.alias} - 胜古朝阳`;
-    }, [categoryInfo?.alias]);
-    return <ContainerNG>
-        <Container right={ width<1200 ? <> </> : <div style={{top:8, position: "sticky"}}>
-                <Feedback />
-            </div>
-        }>
-            {
-                categoryInfo === undefined ?
-                    <div>
-                        <p>
-                            未能找到目录{categoryPath}，你可以尝试：
-                        </p>
-                        <Button
-                            type="primary"
-                            onClick={() => {
-                                const upLevelCategory = categoriesChain.slice(0, categoriesChain.length);
-                                history.push(`/cate/${upLevelCategory.join("/")}`);
-                            }}
-                        >
-                            返回上级目录
-                        </Button>
-                        <Button
-                            type="primary"
-                            onClick={() => history.push("/")}
-                        >
-                            返回主页
-                        </Button>
-                        <Button
-                            type="primary"
-                            onClick={history.goBack}
-                        >
-                            返回上一个页面
-                        </Button>
-                    </div> :
-                    <div className="CategoryPage">
-                        {/* 多级目录导航，未启用 */}
-                        {/* <div>
+  const history = useHistory();
+  const { category: categoryPath } = useParams<{ category: string }>();
+  const categoriesChain = categoryPath.split("/");
+  const { cates } = climbTree(useContext(CateTree));
+  const [width, setWidth] = useState(window.innerWidth);
+  const parseCategory = (): Definitions.CategoryProps | undefined => {
+    const cate = cates.find((cate) => cate.path === categoryPath);
+    return cate;
+  };
+  const [categoryInfo, setCategory] = useState<
+    Definitions.CategoryProps | undefined
+  >(parseCategory());
+  useEffect(() => {
+    setCategory(parseCategory);
+  }, [categoryPath, cates]);
+  useEffect(() => {
+    document.title = `${categoryInfo?.alias} - 胜古朝阳`;
+  }, [categoryInfo?.alias]);
+  return (
+    <Container
+      right={
+        width < 1200 ? (
+          <> </>
+        ) : (
+          <div style={{ top: 8, position: "sticky" }}>
+            <Feedback />
+          </div>
+        )
+      }
+    >
+      {categoryInfo === undefined ? (
+        <div>
+          <p>未能找到目录{categoryPath}，你可以尝试：</p>
+          <Button
+            type="primary"
+            onClick={() => {
+              const upLevelCategory = categoriesChain.slice(
+                0,
+                categoriesChain.length
+              );
+              history.push(`/cate/${upLevelCategory.join("/")}`);
+            }}
+          >
+            返回上级目录
+          </Button>
+          <Button type="primary" onClick={() => history.push("/")}>
+            返回主页
+          </Button>
+          <Button type="primary" onClick={history.goBack}>
+            返回上一个页面
+          </Button>
+        </div>
+      ) : (
+        <div className="CategoryPage">
+          {/* 多级目录导航，未启用 */}
+          {/* <div>
                         {
                             traceToRoot(categoryInfo, cates).map(
                                 (category, index) => <div key={index} onClick={() => history.push(`/cate/${category.path}`)}>
@@ -71,16 +75,14 @@ export const CategoryPage = () => {
                             )
                         }
                     </div> */}
-                        {/* <h2>{categoryInfo.alias}</h2> */}
-                        <div>
-                            {
-                                categoryInfo.childPosts.map(
-                                    (post, index) => <PostCard key={index} post={post}></PostCard>
-                                )
-                            }
-                        </div>
-                        {/* 子目录导航，未启用 */}
-                        {/* <h3>子目录</h3>
+          {/* <h2>{categoryInfo.alias}</h2> */}
+          <div>
+            {categoryInfo.childPosts.map((post, index) => (
+              <PostCard key={index} post={post}></PostCard>
+            ))}
+          </div>
+          {/* 子目录导航，未启用 */}
+          {/* <h3>子目录</h3>
                     <div>
                         {
                             categoryInfo.childCates.map(
@@ -88,8 +90,8 @@ export const CategoryPage = () => {
                             )
                         }
                     </div> */}
-                    </div>
-            }
-        </Container>
-    </ContainerNG>
+        </div>
+      )}
+    </Container>
+  );
 };
