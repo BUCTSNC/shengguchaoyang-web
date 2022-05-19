33f7623d88d8780953e3efda096cf3506ebbc136
Hexiang Qi <qihexiang@outlook.com>
2022-05-19 10:23:20 +0800

    优化了单个JS文件的大小

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:8000"
-    }
-  }
-})
+    plugins: [reactRefresh()],
+    server: {
+        proxy: {
+            "/api": "http://localhost:8000",
+        },
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    antd: ["antd"],
+                    iconpark: ["@icon-park/react"],
+                },
+            },
+        },
+        cssCodeSplit: true
+    },
+});
