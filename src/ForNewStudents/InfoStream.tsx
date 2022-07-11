4ccfde58f3aeed97904a4744d37057208ce07dc1
Kingsdom005 <75762130+Kingsdom005@users.noreply.github.com>
2022-07-11 18:31:25 +0800

    完成部分内容板块

--- src/ForNewStudents/InfoStream.tsx
+++ src/ForNewStudents/InfoStream.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { useContext } from "react";
 import { CateTree } from "../App";
 import { getVisitedCount } from "../dm/hotList";
+// import { useNavigate} from 'react-router';
+// import "./bootstrap.min.css"
 
 export default function InfoStream() {
     const cateTree = useContext(CateTree);
@@ -18,30 +20,34 @@ export default function InfoStream() {
 }
 
 function InfoCard(props: { post: PostProps }) {
+    // const navi = useNavigate()
     const { post } = props;
     return (
-        <div className="info-card">
-            <div className="info-card-title">{post.title}</div>
-            {post.headerImage === undefined ? null : (
-                <div
-                    style={{
-                        width: "100%",
-                        aspectRatio: "16/9",
-                        backgroundImage: `url(/posts/${post.path}/${post.headerImage})`,
-                        backgroundPosition: "center center",
-                        backgroundRepeat: "no-repeat",
-                        backgroundSize: "cover",
-                    }}
-                ></div>
-            )}
-            <div className="info-card-introduction">{post.intro}</div>
-            <div className="info-card-related">
-                {getVisitedCount(`post/${post.path}`)}阅读 ·{" "}
-                {new Date(post.lastModified).toLocaleDateString()}更新
+        <div className="info-card" onClick={()=>{console.log("跳转具体界面")}}>
+            <div className="info-card-title">
+                <span className="info-card-head-dot"></span>
+                <span className="info-card-head-text">{post.title}</span>
             </div>
-            <div className="info-card-tags">
+            {post.headerImage === undefined ? null : (
+            <div
+                style={{
+                    width: "100%",
+                    aspectRatio: "16/9",
+                    backgroundImage: `url(/posts/${post.path}/${post.headerImage})`,
+                    backgroundPosition: "center center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                }}
+            ></div>
+            )}
+            <div className="info-card-introduction" style={{margin:"8px",}}>{post.intro}</div>
+            <div className="info-card-related" style={{margin:"8px",}}>
+                {getVisitedCount(`post/${post.path}`)}阅读 ·{" "}
+                {new Date(post.lastModified).toLocaleDateString().split('/').reverse().join('-')}更新
+            </div>
+            <div className="info-card-tags" style={{margin:"8px"}}>
                 {post.tags?.map((tag, index) => (
-                    <div className="info-card-tag" key={index}>{tag}</div>
+                    <div className="info-card-tag pointer" key={index}><a href="#">{tag}</a></div>
                 ))}
             </div>
         </div>
