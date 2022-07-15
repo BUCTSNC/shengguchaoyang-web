fa0709bae0fbcb13eb23bd2aa462ce8983bea654
Hexiang Qi <qihexiang@outlook.com>
2022-07-15 19:57:11 +0800

    优化时间轴效果

--- src/ForNewStudents/HeaderComponent.tsx
+++ src/ForNewStudents/HeaderComponent.tsx
@@ -159,7 +159,13 @@ function Timeline() {
             title: "军训",
             info: "时间：9:00，地点：体育馆",
         },
-    ];
+    ]
+        .filter((val) => {
+            return (
+                new Date().getTime() - val.date.getTime() <= 24 * 60 * 60 * 1000
+            );
+        })
+        .slice(0, 5);
     const isMobile = useMobileView();
     return isMobile ? (
         <TimeLineMobile data={data} />
@@ -173,14 +179,18 @@ function TimelinePC(props: {
 }) {
     const { data } = props;
     return (
-        <div>
+        <div
+            style={{
+                padding: 16,
+            }}
+        >
             <div
                 style={{
                     display: "grid",
                     gridTemplateColumns: "repeat(10, 1fr)",
-                    gap: "8px",
+                    gap: 8,
                     borderBottom: "2px solid #0054ff",
-                    paddingBottom: "14px",
+                    paddingBottom: 14,
                     minHeight: 192,
                 }}
             >
@@ -329,161 +339,169 @@ function TimelinePC(props: {
     );
 }
 
-function TimeLineMobile(props:{data: { date: Date; title: string; info: string }[]}) {
-    const {data} = props;
-    return         <div>
-    <div
-        style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(6, 1fr)",
-            gap: "8px",
-            borderBottom: "2px solid #0054ff",
-            paddingBottom: "14px",
-            minHeight: 192
-        }}
-    >
+function TimeLineMobile(props: {
+    data: { date: Date; title: string; info: string }[];
+}) {
+    const { data } = props;
+    return (
         <div
             style={{
-                gridArea: "1/1/1/3",
+                padding: 8,
             }}
         >
-            {/* <TimeLineCard {...data[1]} /> */}
-            <div style={{ height: "100%" }}></div>
             <div
                 style={{
-                    height: "8px",
+                    display: "grid",
+                    gridTemplateColumns: "repeat(6, 1fr)",
+                    gap: "8px",
+                    borderBottom: "2px solid #0054ff",
+                    paddingBottom: "14px",
+                    minHeight: 152,
                 }}
-            ></div>
+            >
+                <div
+                    style={{
+                        gridArea: "1/1/1/3",
+                    }}
+                >
+                    {/* <TimeLineCard {...data[1]} /> */}
+                    <div style={{ height: "100%" }}></div>
+                    <div
+                        style={{
+                            height: "8px",
+                        }}
+                    ></div>
+                    <div
+                        style={{
+                            backgroundColor: "#0054ff",
+                            width: "16px",
+                            height: "16px",
+                            borderRadius: "8px",
+                            margin: "auto",
+                        }}
+                    ></div>
+                </div>
+                <div
+                    style={{
+                        gridArea: "1/2/1/4",
+                    }}
+                >
+                    <TimeLineCard {...data[1]} />
+                    <div
+                        style={{
+                            height: "8px",
+                        }}
+                    ></div>
+                    <div
+                        style={{
+                            backgroundColor: "#0054ff",
+                            width: "16px",
+                            height: "16px",
+                            borderRadius: "8px",
+                            margin: "auto",
+                        }}
+                    ></div>
+                </div>
+                <div
+                    style={{
+                        gridArea: "1/3/1/5",
+                    }}
+                >
+                    {/* <TimeLineCard {...data[1]} /> */}
+                    <div style={{ height: "100%" }}></div>
+                    <div
+                        style={{
+                            height: "8px",
+                        }}
+                    ></div>
+                    <div
+                        style={{
+                            backgroundColor: "#0054ff",
+                            width: "16px",
+                            height: "16px",
+                            borderRadius: "8px",
+                            margin: "auto",
+                        }}
+                    ></div>
+                </div>
+                <div
+                    style={{
+                        gridArea: "1/4/1/6",
+                    }}
+                >
+                    <TimeLineCard {...data[3]} />
+                    <div
+                        style={{
+                            height: "8px",
+                        }}
+                    ></div>
+                    <div
+                        style={{
+                            backgroundColor: "#0054ff",
+                            width: "16px",
+                            height: "16px",
+                            borderRadius: "8px",
+                            margin: "auto",
+                        }}
+                    ></div>
+                </div>
+                <div
+                    style={{
+                        gridArea: "1/5/1/7",
+                    }}
+                >
+                    {/* <TimeLineCard {...data[1]} /> */}
+                    <div style={{ height: "100%" }}></div>
+                    <div
+                        style={{
+                            height: "8px",
+                        }}
+                    ></div>
+                    <div
+                        style={{
+                            backgroundColor: "#0054ff",
+                            width: "16px",
+                            height: "16px",
+                            borderRadius: "8px",
+                            margin: "auto",
+                        }}
+                    ></div>
+                </div>
+            </div>
             <div
                 style={{
-                    backgroundColor: "#0054ff",
-                    width: "16px",
-                    height: "16px",
-                    borderRadius: "8px",
-                    margin: "auto",
+                    display: "grid",
+                    gridTemplateColumns: "repeat(6, 1fr)",
+                    gap: "8px",
+                    borderTop: "2px solid #0054ff",
+                    paddingTop: "14px",
+                    minHeight: 152,
                 }}
-            ></div>
+            >
+                <div
+                    style={{
+                        gridColumn: "1/3",
+                    }}
+                >
+                    <TimeLineCard {...data[0]} />
+                </div>
+                <div
+                    style={{
+                        gridColumn: "3/5",
+                    }}
+                >
+                    <TimeLineCard {...data[2]} />
+                </div>
+                <div
+                    style={{
+                        gridColumn: "5/7",
+                    }}
+                >
+                    <TimeLineCard {...data[4]} />
+                </div>
+            </div>
         </div>
-        <div
-            style={{
-                gridArea: "1/2/1/4",
-            }}
-        >
-            <TimeLineCard {...data[1]} />
-            <div
-                style={{
-                    height: "8px",
-                }}
-            ></div>
-            <div
-                style={{
-                    backgroundColor: "#0054ff",
-                    width: "16px",
-                    height: "16px",
-                    borderRadius: "8px",
-                    margin: "auto",
-                }}
-            ></div>
-        </div>
-        <div
-            style={{
-                gridArea: "1/3/1/5",
-            }}
-        >
-            {/* <TimeLineCard {...data[1]} /> */}
-            <div style={{ height: "100%" }}></div>
-            <div
-                style={{
-                    height: "8px",
-                }}
-            ></div>
-            <div
-                style={{
-                    backgroundColor: "#0054ff",
-                    width: "16px",
-                    height: "16px",
-                    borderRadius: "8px",
-                    margin: "auto",
-                }}
-            ></div>
-        </div>
-        <div
-            style={{
-                gridArea: "1/4/1/6",
-            }}
-        >
-            <TimeLineCard {...data[3]} />
-            <div
-                style={{
-                    height: "8px",
-                }}
-            ></div>
-            <div
-                style={{
-                    backgroundColor: "#0054ff",
-                    width: "16px",
-                    height: "16px",
-                    borderRadius: "8px",
-                    margin: "auto",
-                }}
-            ></div>
-        </div>
-        <div
-            style={{
-                gridArea: "1/5/1/7",
-            }}
-        >
-            {/* <TimeLineCard {...data[1]} /> */}
-            <div style={{ height: "100%" }}></div>
-            <div
-                style={{
-                    height: "8px",
-                }}
-            ></div>
-            <div
-                style={{
-                    backgroundColor: "#0054ff",
-                    width: "16px",
-                    height: "16px",
-                    borderRadius: "8px",
-                    margin: "auto",
-                }}
-            ></div>
-        </div>
-    </div>
-    <div
-        style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(6, 1fr)",
-            gap: "8px",
-            borderTop: "2px solid #0054ff",
-            paddingTop: "14px",
-            minHeight: 192,
-        }}
-    >
-        <div
-            style={{
-                gridColumn: "1/3",
-            }}
-        >
-            <TimeLineCard {...data[0]} />
-        </div>
-        <div
-            style={{
-                gridColumn: "3/5",
-            }}
-        >
-            <TimeLineCard {...data[2]} />
-        </div>
-        <div
-            style={{
-                gridColumn: "5/7",
-            }}
-        >
-            <TimeLineCard {...data[4]} />
-        </div>
-    </div>
-</div>
+    );
 }
 
 function TimeLineCard(props: { date: Date; title: string; info: string }) {
